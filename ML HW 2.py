4db7c57aba9a445a29b257fa5365d1b35a4b8fc8
Anthony Burton-Cordova <anthony.burton16@yahoo.com>
2024-03-05 21:10:52 -0600

    Finished Train classifiers function

--- ML HW 2.py
+++ ML HW 2.py
@@ -22,9 +22,6 @@
 # Extra credit.
 # Use Optuna to optimize a NN on a data set.
 
-
-
-
 import numpy as np
 from sklearn.metrics import accuracy_score # Please select some other metircs in addtion to this.
 from sklearn.ensemble import RandomForestClassifier
@@ -32,13 +29,14 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold
 from itertools import product
+from sklearn import datasets
 
 
-#Example data
-M = np.array([[1,2],[3,4],[4,5],[4,5],[4,5],[4,5],[4,5],[4,5]])
-L = np.ones(M.shape[0])
+#Iris data
+iris = datasets.load_iris()
+M = iris.data
+L = iris.target
 n_folds = 5
-
 data = (M, L, n_folds)
 
 clfs = [RandomForestClassifier, GradientBoostingClassifier, LogisticRegression]
@@ -47,7 +45,7 @@ paramDic = {"RandomForestClassifier":{"n_estimators": [100, 200, 500],
             "GradientBoostingClassifier": {"learning_rate": [.1, .01, .5],
                                            "n_estimators": [100, 200, 500]},
             "LogisticRegression":{"fit_intercept": [True, False],
-                                  "penalty": ["l1", "l2", "elasticnet", None]}}
+                                  "penalty": ["l2", None]}}
 
 for model in clfs:
    model_name = str(model).rsplit('.',1)[1][:-2]
@@ -58,33 +56,50 @@ for model in clfs:
     print(arg_dic)
 
 
-def train_classifiers(clf, data);
-  results = {}
-  for clf_name, hyperparams in paramDic.items():
-    #paramDicK = paramDic.keys()
-    #hyperparamsV = paramDic.values()
-    for clf_name, hyperparams in hpnames.
+def train_classifiers(clfs, paramDic, data, n_folds=5):
+    results = {}
+    for clf in clfs:
+        clf_name = clf.__name__
+        hyperparams_list = list(product(*paramDic[clf_name].values()))
+        for hyperparams in hyperparams_list:
+            clf_hyper = dict(zip(paramDic[clf_name].keys(), hyperparams))
+            clf_results = run(clf, data, clf_hyper, n_folds)
+            results[(clf_name, tuple(clf_hyper.items()))] = clf_results
+    return results
+
+            print(results)
 
 
-hyperparamsV = paramDic.values()
-hyperparamsV
+# Assuming 'data' is a tuple (M, L) where M is features and L is labels
+def run(a_clf, data, clf_hyper, n_folds=5):
+    M, L = data
+    kf = KFold(n_splits=n_folds)  # Establish the cross-validation
+    results = []  # Store results from each fold
 
+    for train_index, test_index in kf.split(M):
+        clf = a_clf(**clf_hyper)  # Unpack parameters into clf if they exist
+        clf.fit(M[train_index], L[train_index])
+        pred = clf.predict(M[test_index])
+        accuracy = accuracy_score(L[test_index], pred)
+        results.append(accuracy)
 
+    return sum(results) / len(results), results  # Return average accuracy and accuracy from each fold
 
-def run(a_clf, data, clf_hyper, M, L, n_folds):
-  kf = KFold(n_splits=n_folds) # Establish the cross validation
-  ret = {} # classic explicaiton of results
-  for ids, (train_index, test_index) in enumerate(kf.split(M, L)):
-    clf = a_clf(**clf_hyper) # unpack paramters into clf is they exist
-    clf.fit(M[train_index], L[train_index])
-    pred = clf.predict(M[test_index])
-    ret[ids]= {'clf': clf,
-               'train_index': train_index,
-               'test_index': test_index,
-               'accuracy': accuracy_score(L[test_index], pred)}
-  return ret
+# Example usage
+# Assuming you have your data ready as M (features) and L (labels)
+# M, L = ...
 
-
+# Loop through each classifier and their hyperparameter combinations
+for clf in clfs:
+    clf_name = clf.__name__
+    for arg_values in product(*paramDic[clf_name].values()):
+        arg_dic = dict(zip(paramDic[clf_name].keys(), arg_values))
+        
+        # Example data placeholder, replace with your actual data
+        data = (M, L)
+        avg_accuracy, accuracies = run(clf, data, arg_dic)
+        
+        print(f"Classifier: {clf_name}, Parameters: {arg_dic}, Avg Accuracy: {avg_accuracy}")
 
 results = run(RandomForestClassifier, data, clf_hyper={})
 
